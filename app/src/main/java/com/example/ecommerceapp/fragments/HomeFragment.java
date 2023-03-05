1101d19c6a8607d7d556760cb4ba074a78fbf089
grashibam <86063289+grashibam@users.noreply.github.com>
2023-03-05 23:10:30 +0530

    Update

--- app/src/main/java/com/example/ecommerceapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/ecommerceapp/fragments/HomeFragment.java
@@ -1,9 +1,15 @@
 package com.example.ecommerceapp.fragments;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +18,13 @@ import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.ecommerceapp.R;
+import com.example.ecommerceapp.adapters.CategoryAdapter;
+import com.example.ecommerceapp.models.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +32,14 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView catRecyclerview;
+
+    //Category recyclerview
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
+
+    //FireStore
+    FirebaseFirestore db;
 
 
     public HomeFragment() {
@@ -30,6 +51,9 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        catRecyclerview = root.findViewById(R.id.rec_category);
+
+        db = FirebaseFirestore.getInstance();
 
         //image slider
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
@@ -40,6 +64,30 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.banner3,"Discount", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+
+        // Category
+        catRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryModelList = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(),categoryModelList);
+        catRecyclerview.setAdapter(categoryAdapter);
+
+        db.collection("Category")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                                categoryModelList.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+
+                        }
+                    }
+                });
 
         return root;
     }
